79bed5b31ca692af34c315f836cb944790570857
GrimHub <vladutsava7@yahoo.com>
2020-10-23 22:27:12 +0300

    v.1.3.4

---
+++ Don't Fall/Assets/Assets/Scripts/Music.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+	[SerializeField] private AudioClip[] musicList = null;
+	private AudioSource musicSource;
+
+	private void Awake()
+	{
+		musicSource = GetComponent<AudioSource>();
+	}
+	void Update()
+	{
+		if (musicSource.isPlaying == false)
+		{
+			musicSource.clip = musicList[Random.Range(0, musicList.Length)];
+			musicSource.Play();
+		}
+	}
+}
