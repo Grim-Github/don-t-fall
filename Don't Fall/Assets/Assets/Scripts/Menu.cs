5fc5c91ee4a894ceb6e0c4f415cbaebbf121b998
GrimHub <vladutsava7@yahoo.com>
2020-10-26 14:39:55 +0200

    v2.1.2

--- Don't Fall/Assets/Assets/Scripts/Menu.cs
+++ Don't Fall/Assets/Assets/Scripts/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 public class Menu : MonoBehaviour
@@ -7,9 +8,16 @@ public class Menu : MonoBehaviour
 
 	[SerializeField] private TextMeshProUGUI versionUI = null;
 
+	private static bool once = false;
+
 	private void Awake()
 	{
-		versionUI.text = Application.version;
+		if(once == false && versionUI != null)
+		{
+			versionUI.text = Application.version;
+			once = true;
+		}
+
 	}
 
 	public void QuitGame()
@@ -20,5 +28,58 @@ public class Menu : MonoBehaviour
 	public void ChangeScene(string sceneName)
 	{
 		SceneManager.LoadScene(sceneName);
+	}
+
+	public void AudioVolume()
+	{
+		Slider audioslider = GetComponent<Slider>();
+		AudioListener.volume = audioslider.value;
+	}
+
+	public void ChangeResolutionType()
+	{
+		TMP_Dropdown TMPDD = GetComponent<TMP_Dropdown>();
+		if(TMPDD.value == 0)
+		{
+			Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+		}
+		else if (TMPDD.value == 1)
+		{
+			Screen.fullScreenMode = FullScreenMode.Windowed;
+		}
+		else if (TMPDD.value == 2)
+		{
+			Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+		}
+	}
+
+	public void ChangeResolution()
+	{
+		TMP_Dropdown TMPDD = GetComponent<TMP_Dropdown>();
+		if (TMPDD.value == 0)
+		{
+			Screen.SetResolution(1920,1080 , Screen.fullScreen);
+		}
+		else if (TMPDD.value == 1)
+		{
+			Screen.SetResolution(1600, 900, Screen.fullScreen);
+		}
+		else if (TMPDD.value == 2)
+		{
+			Screen.SetResolution(1280, 720, Screen.fullScreen);
+		}
+	}
+
+	public void ChangeVideo()
+	{
+		TMP_Dropdown TMPDD = GetComponent<TMP_Dropdown>();
+		if (TMPDD.value == 0)
+		{
+			QualitySettings.SetQualityLevel(1);
+		}
+		else if (TMPDD.value == 1)
+		{
+			QualitySettings.SetQualityLevel(0);
+		}
 	}
 }
